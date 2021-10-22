7202cb7039c41da1aa5bf778be47ecbba817dee7
Jonny Alejandro <jonnyalejandro.ca0910@gmail.com>
2021-10-21 19:07:59 -0500

    login daña lo demas

--- controllers/login.php
+++ controllers/login.php
@@ -2,22 +2,24 @@
 require_once __DIR__ . '/../config/config.php';
 require(__DIR__ . '/../classes/BodyOlimpusLogin.php');
 
-if (session_status() === PHP_SESSION_NONE) {
+// if (session_status() === PHP_SESSION_NONE) {
     // $data = extract($_REQUEST);
     // $loging = Login::getLoging($data);
-    $dato = BodyOlimpusLogin::getLoging();
-    if ($dato['exist'] == 1) {
-        $user = $dato['rt']->fetch_object();
-        session_start();
-        $_SESSION['name'] = $user->first_name;
-        header("Location: myaccount");
-    } else {
-        echo "No se encuentra el usuario";
-    }
-} else if (session_status() === PHP_SESSION_ACTIVE) {
+    // $dato = BodyOlimpusLogin::getLoging();
+    // if ($dato['exist'] == 1) {
+    //     $user = $dato['rt']->fetch_object();
+    //     session_start();
+    //     $_SESSION['name'] = $user->first_name;
+    //     header("Location: myaccount");
+    // } else {
+    //     echo "No se encuentra el usuario";
+    // }
+// } else if (session_status() === PHP_SESSION_ACTIVE) {
     // echo $twig->render('myaccount.twig');
-    header("Location: myaccount");
+    // header("Location: myaccount");
     // @session_start();
     // session_destroy();
     // header("Location:".__FILE__);
-}
+// }
+
+echo $twig->render('index.twig');
