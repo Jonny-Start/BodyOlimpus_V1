5b83418115e714f442181f2b39ed2cfa97bd001f
Jonny Alejandro <jonnyalejandro.ca0910@gmail.com>
2021-10-28 17:31:43 -0500

    crear cuenta listo

--- classes/BodyOlimpusLogin.php
+++ classes/BodyOlimpusLogin.php
@@ -11,14 +11,23 @@ class BodyOlimpusLogin
         return $dataUser;
     }
 
-    public static function setUser($email, $pass)
+    public static function setUser($first_name, $last_name, $email, $pass, $createAccount_date)
     {
         require(__DIR__ . './db/DB.php');
 
-        $sql = DB::DBconnect()->prepare("INSERT INTO bo_user (email, passwd) VALUES (:email, :passwd)");
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $pass = $_POST['passwd'];
+        $createAccount_date = date("Y-m-d");
+
+        $sql = DB::DBconnect()->prepare("INSERT INTO bo_user (first_name, last_name, email, passwd, createAccount_date) VALUES (:first_name, :last_name, :email, :passwd, :createAccount_date)");
+        $sql->bindParam(':first_name', $first_name);
+        $sql->bindParam(':last_name', $last_name);
         $sql->bindParam(':email', $email);
         $passwd = password_hash($pass, PASSWORD_BCRYPT);
         $sql->bindParam(':passwd', $passwd);
+        $sql->bindParam(':createAccount_date', $createAccount_date);
         if ($sql->execute()) {
             $message = "Usuario Creado";
         } else {
--- controllers/create_account.php
+++ controllers/create_account.php
@@ -5,12 +5,22 @@ require(__DIR__ . '/../classes/BodyOlimpusLogin.php');
 
 if (!empty($_POST['email']) && !empty($_POST['passwd'])) {
 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $pass = $_POST['passwd'];
+    $createAccount_date = date("Y-m-d");
 
-    $dataUser = BodyOlimpusLogin::setUser($email, $pass);
+
+    $dataUser = BodyOlimpusLogin::setUser($first_name, $last_name, $email, $pass, $createAccount_date);
     if ($dataUser == "Usuario Creado") {
-        echo $twig->render('create_account.twig',compact('dataUser'));
+        $nameView = 'create_account';
+        $message = $dataUser;
+        echo $twig->render('create_account.twig',compact('nameView','message'));
+    }else{
+        $nameView = 'create_account';
+        $message = $dataUser;
+        echo $twig->render('create_account.twig',compact('nameView','message'));
     }
 } else {
     $nameView = 'create_account';
