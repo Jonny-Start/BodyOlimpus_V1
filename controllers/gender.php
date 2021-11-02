0aa1dadf14d044094c9474f3e0f2197713d288e8
Jonny Alejandro <jonnyalejandro.ca0910@gmail.com>
2021-11-02 01:02:56 -0500

    guardando en BD

--- controllers/gender.php
+++ controllers/gender.php
@@ -1,11 +1,29 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
+require(__DIR__ . '/../classes/BodyOlimpusDataUser.php');
 
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login");
     die();
-}else{
-    $nameView = 'bo_gender';
-    echo $twig->render('gender.twig', compact('nameView'));
+} else {
+    if (!empty($_POST['gender'])) {
+
+        $gender = $_POST['gender'];
+        $id_user = $_SESSION['user_id'];
+
+        $dataUser = BodyOlimpusDataUser::updateGender($gender,$id_user);
+        if ($dataUser == "Genero actualizado") {
+            $nameView = 'bo_gender';
+            $message = $dataUser;
+            echo $twig->render('gender.twig', compact('nameView', 'message'));
+        } else {
+            $nameView = 'bo_gender';
+            $message = $dataUser;
+            echo $twig->render('gender.twig', compact('nameView', 'message'));
+        }
+    } else {
+        $nameView = 'bo_gender';
+        echo $twig->render('gender.twig', compact('nameView'));
+    }
 }
