50c5be31feb1b850c8a62ceb025a5fac89576526
Jonny Alejandro <jonnyalejandro.ca0910@gmail.com>
2021-11-09 17:56:01 -0500

    aun no funciona muy bien el perfil

--- classes/BodyOlimpusDataUser.php
+++ classes/BodyOlimpusDataUser.php
@@ -29,16 +29,17 @@ class BodyOlimpusDataUser
         return $message;
     }
 
-    public static function updateDataUserProfile($id_user, $first_name, $last_name, $email, $gender, $phone_number, $exercise_space, $actual_weight, $last_update)
+    public static function updateDataUserProfile($id_user, $first_name, $last_name, $email, $phone_number, $actual_weight, $last_update)
     {
         require(__DIR__ . './db/DB.php');
-        $sql = DB::DBconnect()->prepare("UPDATE bo_user SET first_name = :first_name, last_name = :last_name, email = :email gender = :gender, phone_number = :phone_number, exercise_space = :exercise_space, actual_weight = :actual_weight, last_update = :last_update WHERE bo_user.id_user = :id_user;");
+        $sql = DB::DBconnect()->prepare("UPDATE bo_user SET first_name = :first_name, last_name = :last_name, email = :email, phone_number = :phone_number, actual_weight = :actual_weight, last_update = :last_update WHERE bo_user.id_user = :id_user;");
         $sql->bindParam(':id_user', $id_user);
         $sql->bindParam(':first_name', $first_name);
         $sql->bindParam(':last_name', $last_name);
         $sql->bindParam(':email', $email);
         $sql->bindParam(':phone_number', $phone_number);
-        $sql->bindParam(':exercise_space', $exercise_space);
+        // $sql->bindParam(':gender', $gender);
+        // $sql->bindParam(':exercise_space', $exercise_space);
         $sql->bindParam(':actual_weight', $actual_weight);
         $sql->bindParam(':last_update', $last_update);
         if ($sql->execute()) {
@@ -52,9 +53,9 @@ class BodyOlimpusDataUser
     public static function getDataUserProfile($id_user)
     {
         require(__DIR__ . './db/DB.php');
-        $sql = DB::DBconnect()->prepare("SELECT * FROM `bo_user` WHERE id_user = :id_user");
-        $sql->bindParam(':id_user', $id_user);
-        $data = $sql->execute();
-        return $data;
+        $sql = DB::DBconnect()->query("SELECT * FROM `bo_user` WHERE id_user = $id_user");
+        // $sql->bindParam(':id_user', $id_user);
+        $rows = $sql->fetchAll();
+        return $rows[0];
     }
 }
--- controllers/profile.php
+++ controllers/profile.php
@@ -7,28 +7,28 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login");
     die();
-}else{
+} else {
     if (!empty($_POST['last_update'])) {
 
         $id_user = $_SESSION['user_id'];
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
         $email = $_POST['email'];
-        $gender = $_POST['gender'];
+        // $gender = $_POST['gender'];
         // $edad = $_POST['edad'];
         $phone_number = $_POST['phone_number'];
-        $exercise_space = $_POST['exercise_space'];
+        // $exercise_space = $_POST['exercise_space'];
         $actual_weight = $_POST['actual_weight'];
         $last_update = $_POST['last_update'];
 
-        $dataUser = BodyOlimpusDataUser::updateDataUserProfile($id_user,$first_name,$last_name,$email,$gender,$phone_number,$exercise_space,$actual_weight,$last_update);
-        if ($dataUser == "Datos de usuario actualizado") {
+        $respond = BodyOlimpusDataUser::updateDataUserProfile($id_user, $first_name, $last_name, $email, $phone_number, $actual_weight, $last_update);
+        if ($respond == "Datos de usuario actualizado") {
             $nameView = 'bo_profile';
-            $message = $dataUser;
+            $message = $respond;
             echo $twig->render('profile.twig', compact('nameView', 'message'));
         } else {
             $nameView = 'bo_profile';
-            $message = $dataUser;
+            $message = $respond;
             echo $twig->render('profile.twig', compact('nameView', 'message'));
         }
     } else {
