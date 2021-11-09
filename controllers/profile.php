9d127ad4d396a1927a33d072485c9145c5735098
Jonny Alejandro <jonnyalejandro.ca0910@gmail.com>
2021-11-09 16:57:36 -0500

    creando funcionalidad de profile

--- controllers/profile.php
+++ controllers/profile.php
@@ -1,11 +1,41 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
+require(__DIR__ . '/../classes/BodyOlimpusDataUser.php');
+
 
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login");
     die();
 }else{
-    $nameView = 'bo_profile';
-    echo $twig->render('profile.twig', compact('nameView'));
+    if (!empty($_POST['last_update'])) {
+
+        $id_user = $_SESSION['user_id'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $gender = $_POST['gender'];
+        // $edad = $_POST['edad'];
+        $phone_number = $_POST['phone_number'];
+        $exercise_space = $_POST['exercise_space'];
+        $actual_weight = $_POST['actual_weight'];
+        $last_update = $_POST['last_update'];
+
+        $dataUser = BodyOlimpusDataUser::updateDataUserProfile($id_user,$first_name,$last_name,$email,$gender,$phone_number,$exercise_space,$actual_weight,$last_update);
+        if ($dataUser == "Datos de usuario actualizado") {
+            $nameView = 'bo_profile';
+            $message = $dataUser;
+            echo $twig->render('profile.twig', compact('nameView', 'message'));
+        } else {
+            $nameView = 'bo_profile';
+            $message = $dataUser;
+            echo $twig->render('profile.twig', compact('nameView', 'message'));
+        }
+    } else {
+        $id_user = $_SESSION['user_id'];
+        $date_now = date("Y-m-d");
+        $dataUser = BodyOlimpusDataUser::getDataUserProfile($id_user);
+        $nameView = 'bo_profile';
+        echo $twig->render('profile.twig', compact('nameView', 'dataUser', 'date_now'));
+    }
 }
