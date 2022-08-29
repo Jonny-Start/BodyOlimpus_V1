7233e2f7f9d7c797081d1a887408464ec5f88333
Jonny Alejandro Cano Acosta <jonnyalejandro.ca0910@gmail.com>
2022-08-28 22:06:37 -0500

    iconos

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            //return cache.addAll(["./", "./src/style.css", "./img/logo1.png"]); //en esta linea se cachea lo que esta dentro del arreglo.
-            return cache.addAll(["./views/img/Logo_Transparent.png"]);
-        })
-    );
-});
+// self.addEventListener("install", e => {
+//     e.waitUntil(
+//         caches.open("static").then(cache => {
+//             //return cache.addAll(["./", "./src/style.css", "./img/logo1.png"]); //en esta linea se cachea lo que esta dentro del arreglo.
+//             return cache.addAll(["./views/img/Logo_Transparent.png"]);
+//         })
+//     );
+// });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
-});
+// self.addEventListener("fetch", e => {
+//     e.respondWith(
+//         caches.match(e.request).then(response => {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
